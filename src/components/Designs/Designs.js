16798a4068d9cf825f4d7e083209cf4128252146
killerkc12 <kiranchavankc12@gmail.com>
2021-11-22 23:48:41 +0530

    KFC added in Responsive Design

--- src/components/Designs/Designs.js
+++ src/components/Designs/Designs.js
@@ -19,6 +19,11 @@ import FoodTestimonials from '../../Assets/Design/Food/testimonials.png'
 
 import StarbucksHome from '../../Assets/Design/Starbucks/home.png'
 
+import kfc1 from '../../Assets/Design/KFC/Image 1-min.png'
+import kfc2 from '../../Assets/Design/KFC/Image 2-min.png'
+import kfc3 from '../../Assets/Design/KFC/Image 3-min.png'
+import kfc4 from '../../Assets/Design/KFC/Image 4-min.png'
+
 function Projects() {
 
   return (
@@ -38,6 +43,11 @@ function Projects() {
           <Col md={6}>
               <DesignCards name="Starbucks" img1={StarbucksHome}
                 github="https://github.com/killerkc12/Starbucks" hosted="https://killerkc12.github.io/Starbucks/"
+              />
+          </Col>
+          <Col md={6}>
+            <DesignCards name="KFC" img1={kfc1} img2={kfc2} img3={kfc3} img4={kfc4}
+                github="https://github.com/killerkc12/KFC" hosted="https://killerkc12.github.io/KFC/"
               />
           </Col>
         </Row>
